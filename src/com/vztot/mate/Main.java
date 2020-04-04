4cae8be6e71fb2ada92518f911e264ecc176c678
Oleksandr Husenko <oleksandrhusenko@gmail.com>
2020-04-04 21:43:23 +0300

    removed redundant code

--- src/com/vztot/mate/Main.java
+++ src/com/vztot/mate/Main.java
@@ -1,24 +1,23 @@
 package com.vztot.mate;
 
-import com.vztot.mate.figures.*;
+import com.vztot.mate.figures.Circle;
+import com.vztot.mate.figures.Drawable;
+import com.vztot.mate.figures.Square;
+import com.vztot.mate.figures.Trapeze;
+import com.vztot.mate.figures.Triangle;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
-        generateRandomArrayOfFiguresAndPrint();
-    }
-
-    private static void generateRandomArrayOfFiguresAndPrint() {
-
         int count = (int) (Math.random() * 100 + 1);
         List<Drawable> list = new ArrayList<>();
 
         for (int i = 0; i < count; i++) {
             list.add(getRandomFigure());
         }
-
         for (Drawable figure : list) {
             figure.draw();
         }
@@ -26,40 +25,39 @@ public class Main {
 
     private static Drawable getRandomFigure() {
         String[] colors = {
-                "красный",
-                "оранжевый",
-                "жёлтый",
-                "зелёный",
-                "голубой",
-                "синий",
-                "фиолетовый",
-                "белый",
-                "чёрный"
+                "красный", "оранжевый", "жёлтый",
+                "зелёный", "голубой", "синий",
+                "фиолетовый", "белый", "чёрный"
         };
+
         int randomColorValue = (int) (Math.random() * (colors.length));
         int randomFigureValue = (int) (Math.random() * 4);
 
         switch (randomFigureValue) {
             case 0: {
-                double sideLength = Math.random() * 100 + 1;
-                return new Square(colors[randomColorValue], sideLength);
+                return new Square(colors[randomColorValue],
+                        getRandomDoubleValue());
             }
             case 1: {
-                double hypotenuse = Math.random() * 100 + 1;
-                double heightHeldToHypotenuse = Math.random() * 100 + 1;
-                return new Triangle(colors[randomColorValue], hypotenuse, heightHeldToHypotenuse);
+                return new Triangle(colors[randomColorValue],
+                        getRandomDoubleValue(),
+                        getRandomDoubleValue());
             }
             case 2: {
-                double radius = Math.random() * 100 + 1;
-                return new Circle(colors[randomColorValue], radius);
+                return new Circle(colors[randomColorValue],
+                        getRandomDoubleValue());
             }
             case 3: {
-                double sideA = Math.random() * 100 + 1;
-                double sideB = Math.random() * 100 + 1;
-                double height = Math.random() * 100 + 1;
-                return new Trapeze(colors[randomColorValue], sideA, sideB, height);
+                return new Trapeze(colors[randomColorValue],
+                        getRandomDoubleValue(),
+                        getRandomDoubleValue(),
+                        getRandomDoubleValue());
             }
         }
         return null;
+    }
+
+    static private double getRandomDoubleValue() {
+        return Math.random() * 100 + 1;
     }
 }
